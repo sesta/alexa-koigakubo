29ac82eeb15120dd089e659958dacbeda966c237
sesta <sesta.ec5@gmail.com>
2018-08-25 17:15:29 +0900

    varを駆逐

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 'use strict';
-var Alexa = require('alexa-sdk');
+const Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
-//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = undefined;
+//Make sure to enclose your value in quotes, like this: APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+const APP_ID = undefined;
 
-var SKILL_NAME = "電車の時間";
-var HELP_MESSAGE = "ヘルプのメッセージが入るよ";
-var HELP_REPROMPT = "どうしますか？";
-var STOP_MESSAGE = "さようなら";
+const SKILL_NAME = "電車の時間";
+const HELP_MESSAGE = "ヘルプのメッセージが入るよ";
+const HELP_REPROMPT = "どうしますか？";
+const STOP_MESSAGE = "さようなら";
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+    const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var handlers = {
+const handlers = {
     'LaunchRequest': function () {
-        var message = '電車の時間を教える予定です';
+        const message = '電車の時間を教える予定です';
         this.emit(':tellWithCard', message, SKILL_NAME, message)
     },
     'AMAZON.HelpIntent': function () {
-        var speechOutput = HELP_MESSAGE;
-        var reprompt = HELP_REPROMPT;
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
         this.emit(':ask', speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
