d39a7e6426df6be9e33ac3c1ba9859dc44e0b7c7
sesta <sesta.ec5@gmail.com>
2018-08-25 14:58:45 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
+//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+var APP_ID = undefined;
+
+var SKILL_NAME = "電車の時間";
+var HELP_MESSAGE = "ヘルプのメッセージが入るよ";
+var HELP_REPROMPT = "どうしますか？";
+var STOP_MESSAGE = "さようなら";
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        var message = '電車の時間を教える予定です';
+        this.emit(':tellWithCard', message, SKILL_NAME, message)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'SessionEndedRequest': function () {
+        // Nothing to do
+    }
+};
+
