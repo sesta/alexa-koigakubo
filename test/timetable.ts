c637cc0da1e235f91cd7823301d69b33336485aa
sesta <sesta.ec5@gmail.com>
2018-09-02 11:12:27 +0900

    *.tsのファイルがテストできるようにする part2

---
+++ test/timetable.ts
@@ -0,0 +1,14 @@
+import assert from 'assert'
+
+describe('getRecentTimes', () => {
+  it ('一番近い時刻を取得できる', () => {
+    assert.equal(1, 1)
+  })
+
+  it ('指定した数だけ時刻を取得できる', () => {
+    assert.equal(1, 1)
+  })
+  it("指定した時刻で初期化できる", function(){
+    assert.equal(12, 12);
+  });
+})
