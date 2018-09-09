e4f0d28c258b7704e179883ee549ffee11b4535e
sesta <sesta.ec5@gmail.com>
2018-09-09 21:43:21 +0900

    mainのlintを一通り修正

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as Alexa from 'alexa-sdk'
 import KoigakuboTimetable from './timetable/koigakubo'
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
-//Make sure to enclose your value in quotes, like this: APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1"
 const APP_ID = undefined
 
 const SKILL_NAME = '電車の時間'
@@ -10,7 +8,7 @@ const HELP_MESSAGE = 'ヘルプのメッセージが入るよ'
 const HELP_REPROMPT = 'どうしますか？'
 const STOP_MESSAGE = 'さようなら'
 
-export function handler (event, context, callback) {
+export function handler(event, context, callback) {
   const alexa = Alexa.handler(event, context)
   alexa.APP_ID = APP_ID
   alexa.registerHandlers(handlers)
@@ -18,29 +16,28 @@ export function handler (event, context, callback) {
 }
 
 const handlers = {
-  'LaunchRequest': function () {
+  'LaunchRequest'() {
     const koigakuboTimetable = new KoigakuboTimetable()
     const recentTimes = koigakuboTimetable.getRecentTiems()
-    const timesString = recentTimes.map(time => {
+    const timesString = recentTimes.map((time) => {
       return `${time.hour}時${time.minute}分`
     }).join('、')
 
-    let message: String = `近い順に${timesString}です`
+    const message: string = `近い順に${timesString}です`
     this.emit(':tellWithCard', message, SKILL_NAME, message)
   },
-  'AMAZON.HelpIntent': function () {
+  'AMAZON.HelpIntent'() {
     const speechOutput = HELP_MESSAGE
     const reprompt = HELP_REPROMPT
     this.emit(':ask', speechOutput, reprompt)
   },
-  'AMAZON.CancelIntent': function () {
+  'AMAZON.CancelIntent'() {
     this.emit(':tell', STOP_MESSAGE)
   },
-  'AMAZON.StopIntent': function () {
+  'AMAZON.StopIntent'() {
     this.emit(':tell', STOP_MESSAGE)
   },
-  'SessionEndedRequest': function () {
+  'SessionEndedRequest'() {
     // Nothing to do
-  }
+  },
 }
-
